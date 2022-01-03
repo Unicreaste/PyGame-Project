e1b78ff1a3ee75c614c17bd2368ea46e3e2018d4
STEPA <stepa03102007@gmail.com>
2022-01-03 23:20:57 +0300

    Buttons(without functions)

--- Game.py
+++ Game.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 pygame.init()
 
@@ -6,7 +7,7 @@ display_width = 800
 display_height = 600
 
 gameDisplay = pygame.display.set_mode((display_width, display_height))
-pygame.display.set_caption('A bit Racey')
+pygame.display.set_caption('Pirate robbery')
 clock = pygame.time.Clock()
 
 background = pygame.image.load('Фон.png').convert()
@@ -15,12 +16,70 @@ background = pygame.transform.smoothscale(background, gameDisplay.get_size())
 black = (0, 0, 0)
 white = (255, 255, 255)
 
+
+def load_image(name, color_key=None):
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname).convert()
+    except pygame.error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+
+    if color_key is not None:
+        if color_key == -1:
+            color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
 crashed = False
 
 
+class ButonPlay(pygame.sprite.Sprite):
+    image = load_image('Button_Play.png', color_key=-1)
 
-x = round(display_width * 0.001)
-y = round(display_height * 0.01)
+    def __init__(self, group):
+        super().__init__(group)
+        self.image = ButonPlay.image
+        self.rect = self.image.get_rect()
+        while True:
+            self.rect.topleft = (50, 150)
+            if len(pygame.sprite.spritecollide(self, all_sprites, False)) == 1:
+                break
+
+
+class ButonSettings(pygame.sprite.Sprite):
+    image = load_image('Button_Settings.png', color_key=-1)
+
+    def __init__(self, group):
+        super().__init__(group)
+        self.image = ButonSettings.image
+        self.rect = self.image.get_rect()
+        while True:
+            self.rect.topleft = (50, 250)
+            if len(pygame.sprite.spritecollide(self, all_sprites, False)) == 1:
+                break
+
+class ButonExit(pygame.sprite.Sprite):
+    image = load_image('Button_Exit.png', color_key=-1)
+
+    def __init__(self, group):
+        super().__init__(group)
+        self.image = ButonExit.image
+        self.rect = self.image.get_rect()
+        while True:
+            self.rect.topleft = (50, 350)
+            if len(pygame.sprite.spritecollide(self, all_sprites, False)) == 1:
+                break
+
+
+all_sprites = pygame.sprite.Group()
+
+ButonPlay(all_sprites)
+ButonSettings(all_sprites)
+ButonExit(all_sprites)
 
 run = True
 while run:
@@ -31,17 +90,7 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
-    # update the game states and positions of objects
-    keys = pygame.key.get_pressed()
-    x += (keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]) * 5
-    y += (keys[pygame.K_DOWN] - keys[pygame.K_UP]) * 5
-    x = x % gameDisplay.get_width()
-    y = y % gameDisplay.get_height()
-
     # draw the background
     gameDisplay.blit(background, (0, 0))
-
-    # draw the entire scene
-
-    # update the display
+    all_sprites.draw(gameDisplay)
     pygame.display.flip()
