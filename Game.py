13dbaee52057c52f9867e4af7585ffd8c7b3a762
STEPA <stepa03102007@gmail.com>
2022-01-04 11:12:44 +0300

    Неработает X

--- Game.py
+++ Game.py
@@ -11,7 +11,7 @@ pygame.display.set_caption('Pirate robbery')
 clock = pygame.time.Clock()
 
 pygame.mixer.music.load('Фон(м).mp3')
-pygame.mixer.music.set_volume(10)
+pygame.mixer.music.set_volume(3)
 pygame.mixer.music.play()
 
 background = pygame.image.load('Фон.png').convert()
@@ -40,6 +40,7 @@ def load_image(name, color_key=None):
 
 
 crashed = False
+
 
 class Name(pygame.sprite.Sprite):
     image = load_image('Name.png', color_key=-1)
@@ -92,7 +93,8 @@ class ButonSettings(pygame.sprite.Sprite):
     def get_event(self, event):
         global run
         if self.rect.collidepoint(event.pos):
-            pass
+            SoundButton(all_sprites)
+            ButonX(all_sprites)
 
 
 class ButonExit(pygame.sprite.Sprite):
@@ -113,6 +115,53 @@ class ButonExit(pygame.sprite.Sprite):
             run = False
 
 
+class SoundButton(pygame.sprite.Sprite):
+    image = load_image('S_ON.png', color_key=-1)
+    image2 = load_image('S_OFF.png', color_key=-1)
+    image3 = load_image('S_ON.png', color_key=-1)
+
+    def __init__(self, group):
+        super().__init__(group)
+        self.image = SoundButton.image
+        self.rect = self.image.get_rect()
+        while True:
+            self.rect.topleft = (450, 250)
+            if len(pygame.sprite.spritecollide(self, all_sprites, False)) == 1:
+                break
+
+    def get_event(self, event):
+        global run
+        if self.rect.collidepoint(event.pos):
+            if pygame.mixer.music.get_volume() > 0:
+                pygame.mixer.music.set_volume(0)
+                self.image = self.image2
+            else:
+                pygame.mixer.music.set_volume(10)
+                self.image = self.image3
+
+    def close(self):
+        pass
+
+
+class ButonX(pygame.sprite.Sprite):
+    image = load_image('Bt_X.png', color_key=-1)
+
+    def __init__(self, group):
+        super().__init__(group)
+        self.image = ButonX.image
+        self.rect = self.image.get_rect()
+        while True:
+            self.rect.topleft = (650, 50)
+            if len(pygame.sprite.spritecollide(self, all_sprites, False)) == 1:
+                break
+
+    def get_event(self, event):
+        global run
+        if self.rect.collidepoint(event.pos):
+            self.kill()
+            SoundButton.close(self)
+
+
 all_sprites = pygame.sprite.Group()
 
 ButonPlay(all_sprites)
@@ -123,7 +172,6 @@ Name(all_sprites)
 while run:
     clock.tick(60)
 
-    # handle the events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
